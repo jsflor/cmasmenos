571a98c3b4867ad0ff94f701549eb6c15e9d5d67
Juan Sebastian Flor Usma <50371169+sebastianflor@users.noreply.github.com>
2019-12-30 19:30:49 +0100

    Add files via upload

---
+++ Practica1.cpp
@@ -0,0 +1,14 @@
+/*************************************
+* NOMBRE: #Juan Sebastian#
+* PRIMER APELLIDO: #Flor#
+* SEGUNDO APELLIDO: #Usma#
+* DNI: #54361019J#
+* EMAIL: #Sebastian-flor@hotmail.com#
+*************************************/
+
+#include <stdio.h>
+
+int main(){
+  printf("%s\n%s\n%s\n%s\n%s", "Juan Sebastian", "Flor", "Usma", "54361019J", "Sebastian-flor@hotmail.com");
+  return 0;
+}
